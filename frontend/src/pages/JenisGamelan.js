afbe7351f2075193cca1018fedf8e46b5ef227b1
dianafauziah13 <78580178+dianafauziah13@users.noreply.github.com>
2024-09-04 10:03:17 +0700

    Konten dan kerangka fitur sejarah dan waditra

--- frontend/src/pages/JenisGamelan.js
+++ frontend/src/pages/JenisGamelan.js
@@ -1,11 +1,13 @@
 import React from "react";
+
+
 import { Container, Row, Col } from "react-bootstrap";
 
 function JenisGamelan() {
   return (
     <Container fluid className="about-section">
       <Container>
-        <Row style={{ justifyContent: "center", padding: "10px" }}>
+        {/* <Row style={{ justifyContent: "center", padding: "10px" }}>
           <Col
             md={7}
             style={{
@@ -25,16 +27,47 @@ function JenisGamelan() {
             className="about-img"
           >
           </Col>
-        </Row>
+        </Row> */}
+ 
+      <div className="max-w-sm mx-auto bg-white shadow-lg rounded-2xl overflow-hidden mt-1 mb-8">
+        <div className="p-2">
+          <h2 className="project-heading mb-2 text-black">
+            Jenis-Jenis <strong className="purple">Gamelan</strong>
+          </h2>
+        </div>
+      </div>
+{/* 
         <h1 className="project-heading">
           Professional <strong className="purple">Skillset </strong>
         </h1>
         <h1 className="project-heading">
           <strong className="purple">Tools</strong> I use
-        </h1>
+        </h1> */}
+    <div className="flex flex-col md:flex-row rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700 md:max-w-xl">
+      <img
+          className="h-96 w-full rounded-t-lg object-cover md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
+          src="https://tecdn.b-cdn.net/wp-content/uploads/2020/06/vertical.jpg"
+          alt="" />
+      <div className="flex flex-col justify-start p-6 text-black">
+          <h5
+             className="mb-2 text-xl font-medium text-neutral-800 dark:text-neutral-50">
+            Card title
+          </h5>
+              <p className="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+                  This is a wider card with supporting text below as a natural lead-in
+                  to additional content. This content is a little bit longer.
+              </p>
+              <p className="text-xs text-neutral-500 dark:text-neutral-300">
+                  Last updated 3 mins ago
+              </p>
+        </div>
+    </div>
 
       </Container>
+
+
     </Container>
+    
   );
 }
 
--- frontend/src/pages/Waditra.js
+++ frontend/src/pages/Waditra.js
@@ -5,33 +5,116 @@ function Waditra() {
   return (
     <Container fluid className="about-section">
       <Container>
-        <Row style={{ justifyContent: "center", padding: "10px" }}>
-          <Col
-            md={7}
-            style={{
-              justifyContent: "center",
-              paddingTop: "30px",
-              paddingBottom: "50px",
-            }}
-          >
-            <h1 style={{ fontSize: "2.1em", paddingBottom: "20px" }}>
-              Waditra Gamelan <strong className="purple">I'M</strong>
-            </h1>
+      <div className="w-full max-w-screen-md mx-auto bg-white shadow-lg rounded-2xl overflow-hidden mt-1 mb-4">
+      <div className="p-1">
+        <h3 className="project-heading mb-2 text-black">
+          Waditra <strong className="purple">Gamelan Degung</strong>
+        </h3>
+      </div>
+    </div>
+    
+    <div className="flex flex-col md:flex-row rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700 w-full mb-2">
+      <div className="flex flex-col items-center justify-center w-full md:w-1/3">
+        <img
+          className="h-48 w-48 mt-2 mb-2 object-cover rounded-lg"
+          src="https://tecdn.b-cdn.net/wp-content/uploads/2020/06/vertical.jpg"
+          alt="Saron"
+        />
+      </div>
+      <div className="flex flex-col justify-center p-6 text-left text-black w-full md:w-2/3">
+        <h5 className="mb-2 text-xl font-medium text-neutral-800 dark:text-neutral-50">
+          Saron
+        </h5>
+        <p className="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+        Terdapat dua buah saron (panerus dan peking). Saron adalah waditra jenis alat pukul berbilah, 
+        terbuat dari bahan logam perunggu yang dimainkan dengan cara dipukul, mempergunakan alat bantu pemukul. 
+        saron adalah jenis waditra yang bersuara nyaring/keras. Saron pada Degung berfungsi sebagai pembawa arkuh lagu, 
+        ornamen dan akopayemen. Soepandi, A (1974) saron sebagai variasi dalam lagu.
+        </p>
+      </div>
+    </div>
+    
+    <div className="flex flex-col md:flex-row rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700 w-full mb-2">
+      <div className="flex flex-col items-center justify-center w-full md:w-1/3">
+        <img
+          className="h-48 w-48 mt-2 mb-2 object-cover rounded-lg"
+          src="https://tecdn.b-cdn.net/wp-content/uploads/2020/06/vertical.jpg"
+          alt="Saron"
+        />
+      </div>
+      <div className="flex flex-col justify-center p-6 text-left text-black w-full md:w-2/3">
+        <h5 className="mb-2 text-xl font-medium text-neutral-800 dark:text-neutral-50">
+          Bonang
+        </h5>
+        <p className="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+        Terdapat sebuah bonang dalam Degung. Bonang adalah waditra jenis alat pukul berpenclon, 
+        terbuat dari bahan logam perunggu yang dimainkan dengan cara dipukul mempergunakan alat bantu pemukul. 
+        Bentuk waditra bonang seperti bentuk goong, namun penclonnya berukuran lebih kecil. 
+        Fungsi dari bonang yaitu sebagai pemangku lagu (melodi), piringan, pengatur tempo dalam memperlambat, 
+        mempercepat, menurunkan, memberhentikan gending (Soepandi, 1974).
+        </p>
+      </div>
+    </div>
 
-          </Col>
-          <Col
-            md={5}
-            style={{ paddingTop: "120px", paddingBottom: "50px" }}
-            className="about-img"
-          >
-          </Col>
-        </Row>
-        <h1 className="project-heading">
-          Professional <strong className="purple">Skillset </strong>
-        </h1>
-        <h1 className="project-heading">
-          <strong className="purple">Tools</strong> I use
-        </h1>
+    <div className="flex flex-col md:flex-row rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700 w-full mb-2">
+      <div className="flex flex-col items-center justify-center w-full md:w-1/3">
+        <img
+          className="h-48 w-48 mt-2 mb-2 object-cover rounded-lg"
+          src="https://tecdn.b-cdn.net/wp-content/uploads/2020/06/vertical.jpg"
+          alt="Saron"
+        />
+      </div>
+      <div className="flex flex-col justify-center p-6 text-left text-black w-full md:w-2/3">
+        <h5 className="mb-2 text-xl font-medium text-neutral-800 dark:text-neutral-50">
+          Jengglong
+        </h5>
+        <p className="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+        Jengglong adalah waditra berpenclon dibuat dari perunggu, kuningan atau besi yang berdiameter 
+        antara tiga puluh sampai dengan empat puluh centimeter. Dalam suatu ancak atau kakanco terdiri atas enam buah kromong.
+        </p>
+      </div>
+    </div>
+
+    <div className="flex flex-col md:flex-row rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700 w-full mb-2">
+      <div className="flex flex-col items-center justify-center w-full md:w-1/3">
+        <img
+          className="h-48 w-48 mt-2 mb-2 object-cover rounded-lg"
+          src="https://tecdn.b-cdn.net/wp-content/uploads/2020/06/vertical.jpg"
+          alt="Saron"
+        />
+      </div>
+      <div className="flex flex-col justify-center p-6 text-left text-black w-full md:w-2/3">
+        <h5 className="mb-2 text-xl font-medium text-neutral-800 dark:text-neutral-50">
+          Penclon
+        </h5>
+        <p className="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+        Penclon pada alat musik jengglong berjumlah enam buah yang terdiri dari nada lima hingga lima 
+        di bawahnya dengan wilayah nada yang lebih rendah dari bonang. Penclon-penclon ini digantung dengan 
+        tali pada penyangga yangmberbentuk tiang gantungan. Soepandi, A (1974) jenglong berfungsi sebagai rangka gending.
+        </p>
+      </div>
+    </div>
+
+    <div className="flex flex-col md:flex-row rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700 w-full mb-2">
+      <div className="flex flex-col items-center justify-center w-full md:w-1/3">
+        <img
+          className="h-48 w-48 mt-2 mb-2 object-cover rounded-lg"
+          src="https://tecdn.b-cdn.net/wp-content/uploads/2020/06/vertical.jpg"
+          alt="Saron"
+        />
+      </div>
+      <div className="flex flex-col justify-center p-6 text-left text-black w-full md:w-2/3">
+        <h5 className="mb-2 text-xl font-medium text-neutral-800 dark:text-neutral-50">
+          Goong
+        </h5>
+        <p className="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+        Goong adalah waditra jenis alat pukul berpenclon, terbuat dari bahan logam perunggu. 
+        Dibunyikan dengan cara dipukul oleh alat bantu pemukul dang menghasilkan suara yang paling besar. 
+        Soepandi (1974) fungsi goong dalam Degung yaitu sebagai barometer lagu. Bertugas sebagai pengatur wiletan 
+        (birama) atau sebagai tanda akhir periode melodi dan penutup kalimat lagu.
+        </p>
+      </div>
+    </div>
 
       </Container>
     </Container>
