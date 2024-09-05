275a8cd3a0e8321c62fea97a50b82b43ec5b868f
dianafauziah13 <78580178+dianafauziah13@users.noreply.github.com>
2024-09-05 08:25:14 +0700

    Kerangka Notasi

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -57,7 +57,7 @@ function NavBar() {
             <Nav.Item>
               <Nav.Link
                 as={Link}
-                to="/project"
+                to="/notasi"
                 onClick={() => updateExpanded(false)}
                 style={{ marginBottom: "2px" }}
               >
--- frontend/src/pages/Notasi.js
+++ frontend/src/pages/Notasi.js
@@ -2,36 +2,66 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 
 function Notasi() {
+  const [showModal, setShowModal] = React.useState(false);
   return (
     <Container fluid className="about-section">
       <Container>
-        <Row style={{ justifyContent: "center", padding: "10px" }}>
-          <Col
-            md={7}
-            style={{
-              justifyContent: "center",
-              paddingTop: "30px",
-              paddingBottom: "50px",
-            }}
-          >
-            <h1 style={{ fontSize: "2.1em", paddingBottom: "20px" }}>
-              Notasi Gamelan <strong className="purple">I'M</strong>
-            </h1>
+      <div className="max-w-sm mx-auto bg-white shadow-lg rounded-2xl overflow-hidden mt-1 mb-8">
+        <div className="p-2">
+          <h2 className="project-heading mb-2 text-black">
+            Notasi <strong className="purple">Lagu</strong>
+          </h2>
+        </div>
+      </div>
 
-          </Col>
-          <Col
-            md={5}
-            style={{ paddingTop: "120px", paddingBottom: "50px" }}
-            className="about-img"
+        <>
+        <button
+        className="bg-pink-500 rounded-full text-white active:bg-pink-600 font-bold uppercase text-sm w-100 h-10 flex items-center justify-center shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+        type="button"
+        onClick={() => setShowModal(true)}
+        >
+         Notasi Lagu 1
+        </button>
+
+      {showModal ? (
+        <>
+          <div
+            className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
           >
-          </Col>
-        </Row>
-        <h1 className="project-heading">
-          Professional <strong className="purple">Skillset </strong>
-        </h1>
-        <h1 className="project-heading">
-          <strong className="purple">Tools</strong> I use
-        </h1>
+            <div className="relative w-auto my-6 mx-auto max-w-3xl">
+              {/*content*/}
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                {/*header*/}
+                <div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
+                  <h3 className="text-3xl font-semibold text-black">
+                    Notasi Lagu
+                  </h3>
+                </div>
+                {/*body*/}
+                <div className="relative p-6 flex-auto">
+              <img
+                src="https://tecdn.b-cdn.net/wp-content/uploads/2020/06/vertical.jpg"
+                alt="Notasi Lagu"
+                className="h-95 w-80"
+              />
+            </div>
+                {/*footer*/}
+                <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
+                  <button
+                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={() => setShowModal(false)}
+                  >
+                    Close
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+        </>
+      ) : null}
+    </>
 
       </Container>
     </Container>
