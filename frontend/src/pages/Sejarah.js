5f96839d0b58e60f69a7ac53490b04ec78e94e6c
dianafauziah13 <78580178+dianafauziah13@users.noreply.github.com>
2024-10-08 07:59:56 +0700

    perbarui 2

--- frontend/src/pages/Sejarah.js
+++ frontend/src/pages/Sejarah.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
+import gamelan1 from "../Assets/Projects/gamelan_1.png";
+import gamelan2 from "../Assets/Projects/gamelan_2.png";
 
 function About() {
   return (
@@ -16,7 +18,7 @@ function About() {
       <div className="flex flex-col items-center justify-center w-full md:w-1/3">
         <img
           className="h-48 w-48 mt-2 object-cover rounded-lg"
-          src="https://tecdn.b-cdn.net/wp-content/uploads/2020/06/vertical.jpg"
+          src={gamelan1}
           alt="Saron"
         />
         <p className="mt-2 text-white text-xs text-neutral-500 dark:text-neutral-300">
@@ -59,7 +61,7 @@ function About() {
       <div className="flex flex-col items-center justify-center w-full md:w-1/3">
         <img
           className="h-48 w-48 mt-2 object-cover rounded-lg"
-          src="https://tecdn.b-cdn.net/wp-content/uploads/2020/06/vertical.jpg"
+          src={gamelan2}
           alt="Saron"
         />
          <p className="mt-2 text-white text-xs text-center text-neutral-500 dark:text-neutral-300 md:text-sm lg:text-base">
